7c02144ae1248e2386e6a44d4057d089d6f87bfe
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-16 15:02:29 -0800

    added tests and npm i testing library

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
@@ -7,5 +7,50 @@ describe('App component', () => {
   it('renders App', () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
+  });
+    
+  it('checks the current color', async() => {
+    render(<App />);
+    const current = screen.getByTestId('current');
+    fireEvent.change(current, {
+      target: {
+        value: '#FF0000'
+      }
+    });
+    const display = await screen.findByTestId('display');
+        
+    expect(display).toHaveStyle({
+      backgroundColor: '#FF0000',
+    });
+  });
+
+  it('checks the current color after hitting the undo button', async() => {
+    render(<App />);
+    const current = screen.getByTestId('before');
+    fireEvent.change(current, {
+      target: {
+        value: '#00FF00'
+      }
+    });
+    const display = await screen.findByTestId('display');
+        
+    expect(display).toHaveStyle({
+      backgroundColor: '#FF0000',
+    });
+  });
+
+  it('checks the current color after hitting the redo button', async() => {
+    render(<App />);
+    const current = screen.getByTestId('after');
+    fireEvent.change(current, {
+      target: {
+        value: '#FF0000'
+      }
+    });
+    const display = await screen.findByTestId('display');
+        
+    expect(display).toHaveStyle({
+      backgroundColor: '#FF0000',
+    });
   });
 });
