48ce0f3ac69255f88e24fb47a09afd225c179937
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-16 17:20:19 -0800

    reducer is working

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,45 +1,19 @@
-import React, { useState } from 'react';
-
-const useRecord = (init) => {
-  const [before, setBefore] = useState([]);
-  const [current, setCurrent] = useState(init);
-  const [after, setAfter] = useState([]);
-
-  const undo = () => {
-    setAfter(after => [current, ...after]);
-    setCurrent(before[before.length - 1]);
-    setBefore(before => before.slice(0, -1));
-  };
-
-  const redo = () => {
-    setBefore(before => [...before, current]);
-    setCurrent(after[0]);
-    setAfter(after => after.slice(1));
-  };
-
-  const record = val => {
-    setBefore(before => [...before, current]);
-    setCurrent(val);
-  };
-
-  return {
-    undo,
-    record,
-    redo,
-    current,
-  };
-};
+import React, { useReducer, useState } from 'react';
+import colorReducer,{ initialState } from '../reducer/Reducer';
 
 function App() {
-  const { current, undo, redo, record } = useRecord('#FF0000');
+  const [state, dispatch] = useReducer(colorReducer, initialState);
+  const undo = () => {dispatch({ type: 'undo'});};
+  const redo = () => {dispatch({ type: 'redo'});};
+
 
   return (
     <>
-      <button data-testid="before" onClick={undo}>undo</button>
-      <button data-testid="after" onClick={redo}>redo</button>
-      <input data-testid="current" type="color" value={current}
-        onChange={({ target }) => record(target.value)} />
-      <div  data-testid="display" style={{ backgroundColor: current, width: '10rem', height: '10rem' }}>
+      <button disabled={!state.before.length} data-testid="before" onClick={undo}>undo</button>
+      <button disabled={!state.after.length} data-testid="after" onClick={redo}>redo</button>
+      <input data-testid="current" type="color" id="current" value={state.current}
+        onChange={({ target }) => {dispatch({type: target.id, payload: target.value })}} />
+      <div  data-testid="display" style={{ backgroundColor: state.current, width: '10rem', height: '10rem' }}>
       </div>
     </>
   );
